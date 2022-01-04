5df4fbe78fc9d6e67e268f6980925be6d2f862ce
Mohammad Arif <arif_iq@yahoo.co.in>
2022-01-04 23:18:37 +0530

    Added Navigation Bar and deleted the redundant zip file.

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Profile from './pages/Profile'
 import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
 import ForgotPassword from './pages/ForgotPassword'
+import Navbar from './components/Navbar'
 
 function App () {
   return (
@@ -13,13 +14,13 @@ function App () {
         <Routes>
           <Route path='/' element={<Explore />} />
           <Route path='/offers' element={<Offers />} />
-          <Route path='/profile' element={<SignIn />} />
+          <Route path='/profile' element={<Profile />} />
           <Route path='/sign-in' element={<SignIn />} />
           <Route path='/sign-up' element={<SignUp />} />
           <Route path='/forgot-password' element={<ForgotPassword />} />
         </Routes>
+        <Navbar />
       </Router>
-      {/* Navbar */}
     </>
   )
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,38 @@
+import { useNavigate, useLocation } from "react-router-dom"
+import {ReactComponent as OfferIcon} from '../assets/svg/localOfferIcon.svg'
+import {ReactComponent as ExploreIcon} from '../assets/svg/exploreIcon.svg'
+import {ReactComponent as PersonOutlineIcon} from '../assets/svg/personOutlineIcon.svg'
+
+function Navbar() {
+  const navigate = useNavigate()  // useNavigate() hook returns a function that lets you navigate programmatically
+  const location = useLocation()  // This hook returns the current location object. This can be useful if you'd like to perform some side effect whenever the current location changes.
+
+  const pathMatchRoute = (route) => {
+    if(route === location.pathname) {
+      return true
+    }
+  }
+
+  return (
+    <footer className="navbar">
+      <nav className="navbarNav">
+        <ul className="navbarListItems">
+          <li className="navbarListItem" onClick={() => navigate('/')}>
+            <ExploreIcon fill={pathMatchRoute('/') ? '#2c2c2c': '#8f8f8f'} width='36px' height='36px' />
+            <p className={pathMatchRoute('/') ? 'navbarListItenNameActive' : 'navbarListItemName' }>Explore</p>
+          </li>
+          <li className="navbarListItem" onClick={() => navigate('/offers')}>
+            <OfferIcon fill={pathMatchRoute('/offers') ? '#2c2c2c': '#8f8f8f'} width='36px' height='36px' />
+            <p className={pathMatchRoute('/offers') ? 'navbarListItenNameActive' : 'navbarListItemName' }>Offer</p>
+          </li>
+          <li className="navbarListItem" onClick={() => navigate('/profile')}>
+            <PersonOutlineIcon fill={pathMatchRoute('/profile') ? '#2c2c2c': '#8f8f8f'} width='36px' height='36px' />
+            <p className={pathMatchRoute('/profile') ? 'navbarListItenNameActive' : 'navbarListItemName' }>Profile</p>
+          </li>
+        </ul>
+      </nav>
+    </footer>
+  )
+}
+
+export default Navbar
